d46f45f3f21b1289a4aede2620cd390a18c4f901
KojoBoat <egyirchristian07@gmail.com>
2020-11-13 17:18:16 +0000

    Source file

---
+++ src/test/java/com/chris/springsoap/SpringsoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.springsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
